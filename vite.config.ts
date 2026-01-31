f596c84039b196fd2ee8c0a23be2e92ae3831449
Younis Amin <younisa1002@gmail.com>
2026-01-31 22:58:20 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
+import react from "@vitejs/plugin-react"
+import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [inspectAttr(), react()],
+  base: "/", // required for muhammadyounis.github.io
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
